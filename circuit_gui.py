2120c71db7f7bc81cb262c6d24ec5856d45db8ae
Xiaoyue Zhu <xz1634@nyu.edu>
2020-02-08 00:02:58 +0800

    add popup error message

--- circuit_gui.py
+++ circuit_gui.py
@@ -9,26 +9,23 @@ import numpy as np
 import os
 import sys
 
-curr_dir = os.path.dirname(os.path.realpath('__file__'))
-os.chdir(curr_dir)
-
 # load workout dataframe
-df = pd.read_csv("~/circuit_maker/workouts.csv")
+df = pd.read_csv(os.path.join(sys.path[0], "workouts.csv"))
 
 # load the beeps
-single_beep = "~/circuit_maker/single_beep.mp3"
+single_beep = os.path.join(sys.path[0], "single_beep.mp3")
 
 class Circuits(object):
     # the welcome window
     def init(self, root):
         # default values
-        self.emphasis = "Abs" # primary focus
-        self.sec_emphasis = "Butt" # secondary focus
+        #self.emphasis = "Abs" # primary focus
+        #self.sec_emphasis = "Butt" # secondary focus
         self.emp_prob = 0.5  # chosen probabiliy for the primary focus
         self.sec_emp_prob = 0.3 # chosen probability for the secondary focus
-        self.intensity = 1 # low intensity 
-        self.weight = 1 # has weights
-        self.band = 1 # has band
+        #self.intensity = 2 # medium intensity 
+        #self.weight = 1 # has weights
+        #self.band = 1 # has band
 
         TESTING = 0 # for testing 
         if TESTING == 1:
@@ -73,23 +70,20 @@ class Circuits(object):
 
     def circuit_maker(self, num_circ, num_sets, intensity, 
                     emphasis, sec_emphasis, emp_prob, sec_emp_prob):
-        
-        # intensity
-        if intensity < 3:
-            intensity_min = 0
-            intensity_max = intensity
-        else:
-            intensity_min = 1
-            intensity_max = intensity
-            
+                    
         # determine body parts and their chosen probability 
         body_parts = ['Arms','Legs','Butt','Abs','Back']
         emp_inx = [i for i in range(len(body_parts)) if body_parts[i] == emphasis][0]
         sec_emp_inx = [i for i in range(len(body_parts)) if body_parts[i] == sec_emphasis][0]
-        others_prob = (1 - emp_prob - sec_emp_prob) / (len(body_parts) - 2)
-        prob_list = np.repeat(others_prob, len(body_parts))
-        prob_list[emp_inx] = emp_prob
-        prob_list[sec_emp_inx] = sec_emp_prob
+        if emp_inx == sec_emp_inx: # just in case 
+            others_prob = (1 - emp_prob - sec_emp_prob) / (len(body_parts) - 1)
+            prob_list = np.repeat(others_prob, len(body_parts))
+            prob_list[emp_inx] = emp_prob + sec_emp_prob
+        else:
+            others_prob = (1 - emp_prob - sec_emp_prob) / (len(body_parts) - 2)
+            prob_list = np.repeat(others_prob, len(body_parts))
+            prob_list[emp_inx] = emp_prob
+            prob_list[sec_emp_inx] = sec_emp_prob
 
         # choose workouts based on criteria without replacements
         circuits = pd.DataFrame()
@@ -99,35 +93,16 @@ class Circuits(object):
                 this_workout = df[(df.Type == 'Cardio') & (df.chosen == 0)].sample()
                 df.chosen[this_workout.index] = 1 # tag this workout
                 circuits = circuits.append(this_workout)
-                #posture = np.random.choice(['lying_belly','lying_back'], p = [1/2, 1/2]) # posture of the latter half 
-            elif i % num_sets == 1: # if this is the second workout, choose a workout with standing posture
-                try:
-                    if self.weight and self.band:
-                        this_workout = df[(df.Type != 'Cardio') & (df.chosen == 0) & (df.Posture == 'standing')].sample()
-                    elif self.weight and not self.band:
-                        this_workout = df[(df.Type != 'Cardio') & (df.Band == 0) & (df.chosen == 0) & (df.Posture == 'standing')].sample()
-                    elif not self.weight and self.band:
-                        this_workout = df[(df.Type != 'Cardio') & (df.Weight == 0) & (df.chosen == 0) & (df.Posture == 'standing')].sample()
-                    else:
-                        this_workout = df[(df.Type != 'Cardio') & (df.Band == 0) & (df.Weight == 0) & (df.chosen == 0) & (df.Posture == 'standing')].sample()
-                    df.chosen[this_workout.index] = 1
-                    circuits = circuits.append(this_workout)
-                except:
-                    print('Not enough non-repeating workouts')
             else:           
                 try:
                     if self.weight and self.band:
-                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity_max) 
-                                        & (df.Intensity > intensity_min) & (df.chosen == 0)].sample()
+                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity) & (df.chosen == 0)].sample()
                     elif self.weight and not self.band:
-                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity_max) 
-                                        & (df.Intensity > intensity_min) & (df.Band == 0) & (df.chosen == 0)].sample()
+                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity) & (df.Band == 0) & (df.chosen == 0)].sample()
                     elif not self.weight and self.band:
-                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity_max) 
-                                        & (df.Intensity > intensity_min) & (df.Weight == 0) & (df.chosen == 0)].sample()
-                    else:
-                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity_max) 
-                                        & (df.Intensity > intensity_min) & (df.Band == 0) & (df.Weight == 0) & (df.chosen == 0)].sample()
+                        this_workout = df[(df[chosen_type] == 1) & (df.Intensity <= intensity) & (df.Weight == 0) & (df.chosen == 0)].sample()
+                    else: # when there is no weight nor band, intensity constraint can be relaxed
+                        this_workout = df[(df[chosen_type] == 1) & (df.Band == 0) & (df.Weight == 0) & (df.chosen == 0)].sample()
                     df.chosen[this_workout.index] = 1
                     circuits = circuits.append(this_workout)
                 except:
@@ -146,81 +121,68 @@ class Circuits(object):
         title.grid(row = 0, columnspan = 9, pady = 8)
 
         circ_response = tk.Label(self.frame, text = "Number of circuits:", font = ("Avenir", 20), justify = 'left')
-        circ_response.grid(row = 1, column = 0, padx = 5, pady = 5)
+        circ_response.grid(row = 1, column = 0, padx = 20, pady = 5, sticky = tk.W)
 
         set_response = tk.Label(self.frame, text = "Number of sets:", font = ("Avenir", 20), justify = 'left')
-        set_response.grid(row = 2, column = 0, padx = 5, pady = 5)
+        set_response.grid(row = 2, column = 0, padx = 20, pady = 5, sticky = tk.W)
 
         repeat_response = tk.Label(self.frame, text = "Number of repeats:", font = ("Avenir", 20), justify = 'left')
-        repeat_response.grid(row = 3, column = 0, padx = 5, pady = 5) 
+        repeat_response.grid(row = 3, column = 0, padx = 20, pady = 5, sticky = tk.W) 
 
         self.circ = tk.StringVar(value = self.circ_val)
         circ_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.circ, width = 5)
-        circ_proposal.grid(row = 1, column = 1, padx = 5, pady = 5)
+        circ_proposal.grid(row = 1, column = 1, padx = 5, pady = 5, sticky = tk.W)
 
         self.set = tk.StringVar(value = self.set_val)
         set_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.set, width = 5)
-        set_proposal.grid(row = 2, column = 1, padx = 5, pady = 5)
+        set_proposal.grid(row = 2, column = 1, padx = 5, pady = 5, sticky = tk.W)
 
         self.repeat = tk.StringVar(value = self.repeat_val)
         repeat_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.repeat, width = 5)
-        repeat_proposal.grid(row = 3, column = 1, padx = 5, pady = 5)
+        repeat_proposal.grid(row = 3, column = 1, padx = 5, pady = 5, sticky = tk.W)
 
         emp_response = tk.Label(self.frame, text = "Primary focus:", font = ("Avenir", 20), justify = 'left')
-        emp_response.grid(row = 4, column = 0, padx = 5, pady = 5)
+        emp_response.grid(row = 4, column = 0, padx = 20, pady = 5, sticky = tk.W)
         
-        self.v1 = tk.IntVar(None, 1)
-        radio_values = {"Arms": 1,"Legs": 2,"Butt": 3,"Abs": 4, "Back": 5, "Random":6}
-        for (text, value) in radio_values.items(): 
-            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v1, command = self.emp_chosen, font = ("Avenir", 20)).grid(row = 4, column = int(value), padx = 5, pady = 5)
-
-        sec_emp_response = tk.Label(self.frame, text = "Secondary focus:", font = ("Avenir", 20), justify = 'left')
-        sec_emp_response.grid(row = 5, column = 0, padx = 5, pady = 5)
-        
-        self.v7 = tk.IntVar(None, 1)
+        self.v1 = tk.IntVar(None)
         radio_values = {"Arms": 1,"Legs": 2,"Butt": 3,"Abs": 4, "Back": 5}
         for (text, value) in radio_values.items(): 
-            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v7, command = self.sec_emp_chosen, font = ("Avenir", 20)).grid(row = 5, column = int(value), padx = 5, pady = 5)
+            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v1, command = self.emp_chosen, font = ("Avenir", 20)).grid(row = 4, column = int(value), padx = 5, pady = 5, sticky = tk.W)
+
+        sec_emp_response = tk.Label(self.frame, text = "Secondary focus:", font = ("Avenir", 20), justify = 'left')
+        sec_emp_response.grid(row = 5, column = 0, padx = 20, pady = 5, sticky = tk.W)
+        
+        self.v7 = tk.IntVar(None)
+        radio_values = {"Arms": 1,"Legs": 2,"Butt": 3,"Abs": 4, "Back": 5}
+        for (text, value) in radio_values.items(): 
+            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v7, command = self.sec_emp_chosen, font = ("Avenir", 20)).grid(row = 5, column = int(value), padx = 5, pady = 5, sticky = tk.W)
 
         intensity_response = tk.Label(self.frame, text = "Training intensity:", font = ("Avenir", 20), justify = 'left')
-        intensity_response.grid(row = 6, column = 0, padx = 5, pady = 5)
+        intensity_response.grid(row = 6, column = 0, padx = 20, pady = 5, sticky = tk.W)
 
-        self.v2 = tk.IntVar(None, 1)
-        radio_values = {"Low": 1,"Medium": 2,"High": 3}
+        self.v2 = tk.IntVar(None)
+        radio_values = {"Medium": 1,"High": 2}
         for (text, value) in radio_values.items(): 
-            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v2, command = self.int_chosen, font = ("Avenir", 20)).grid(row = 6, column = int(value), padx = 5, pady = 5)
+            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v2, command = self.int_chosen, font = ("Avenir", 20)).grid(row = 6, column = int(value), padx = 5, pady = 5, sticky = tk.W)
 
         weight_response = tk.Label(self.frame, text = "Weights?", font = ("Avenir", 20), justify = 'left')
-        weight_response.grid(row = 7, column = 0, padx = 5, pady = 5)
+        weight_response.grid(row = 7, column = 0, padx = 20, pady = 5, sticky = tk.W)
 
         band_response = tk.Label(self.frame, text = "Resistance band?", font = ("Avenir", 20), justify = 'left')
-        band_response.grid(row = 8, column = 0, padx = 5, pady = 5)
+        band_response.grid(row = 8, column = 0, padx = 20, pady = 5, sticky = tk.W)
 
-        self.v3 = tk.IntVar(None, 1)
+        self.v3 = tk.IntVar(None)
         yes_no = {"Yes": 1, "No": 2}
         for (text, value) in yes_no.items():
-            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v3, command = self.weight_chosen, font = ("Avenir", 20)).grid(row = 7, column = int(value), padx = 5, pady = 5)
+            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v3, command = self.weight_chosen, font = ("Avenir", 20)).grid(row = 7, column = int(value), padx = 5, pady = 5, sticky = tk.W)
 
-        self.v4 = tk.IntVar(None, 1)
+        self.v4 = tk.IntVar(None)
         for (text, value) in yes_no.items():
-            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v4, command = self.band_chosen, font = ("Avenir", 20)).grid(row = 8, column = int(value), padx = 5, pady = 5)
+            tk.Radiobutton(self.frame, text = text, value = value, variable = self.v4, command = self.band_chosen, font = ("Avenir", 20)).grid(row = 8, column = int(value), padx = 5, pady = 5, sticky = tk.W)
 
-        next_button = tk.Button(self.frame, text = 'Next', font = ("Avenir", 20), justify = 'center', command = self.window_2)
+        next_button = tk.Button(self.frame, text = 'Next', font = ("Avenir", 20), justify = 'center', command = self.circuit_wrapper)
         next_button.grid(row = 9, column = 7)
 
-        # check if the specification results in enough non-repeating workouts
-        #total_workouts = int(self.circ.get()) * (int(self.set.get()) - 1) # def have enough cardios
-        #if self.weight and self.band:
-        #    resulting_workouts = df[(df[self.emphasis] == 1) & (df.Intensity <= self.intensity)].shape[0]
-        #elif self.weight and not self.band:
-        #    resulting_workouts = df[(df[self.emphasis] == 1) & (df.Intensity <= self.intensity) & (df.Band == 0)].shape[0]
-        #elif not self.weight and self.band:
-        #    resulting_workouts = df[(df[self.emphasis] == 1) & (df.Intensity <= self.intensity) & (df.Weight == 0)].shape[0]
-        #elif not self.weight and not self.band:
-        #    resulting_workouts = df[(df[self.emphasis] == 1) & (df.Intensity <= self.intensity) & (df.Weight == 0) & (df.Band == 0)].shape[0]
-        #if resulting_workouts < total_workouts:
-            #tk.tkMessageBox.showinfo("Error", "Not enough non-repeating workouts!")
-            
     # primary focus button commands
     def emp_chosen(self):
         body_parts = ['Arms','Legs','Butt','Abs','Back']
@@ -233,7 +195,7 @@ class Circuits(object):
 
     # training intensity button commands
     def int_chosen(self):
-        intensities = ['Low', 'Medium', 'High']
+        intensities = ['Medium', 'High']
         self.intensity = intensities[self.v2.get() - 1]
 
     # weight yes-no radio button commands
@@ -260,41 +222,41 @@ class Circuits(object):
         title.grid(row=0, columnspan=9, pady=8)
 
         ht_response = tk.Label(self.frame, text = "High Intensity:", font = ("Avenir", 20), justify = 'left')
-        ht_response.grid(row = 1, column = 1, pady = 5, padx = 5)
+        ht_response.grid(row = 1, column = 1, pady = 5, padx = 20, sticky = tk.W)
 
         lt_response = tk.Label(self.frame, text = "Low Intensity:", font = ("Avenir", 20), justify = 'left')
-        lt_response.grid(row = 2, column = 1, pady = 5, padx = 5)
+        lt_response.grid(row = 2, column = 1, pady = 5, padx = 20, sticky = tk.W)
 
         between_response = tk.Label(self.frame, text = "Between Circuits:", font = ("Avenir", 20), justify = 'left')
-        between_response.grid(row = 3, column = 1, pady = 5, padx = 5)
+        between_response.grid(row = 3, column = 1, pady = 5, padx = 20, sticky = tk.W)
 
         warmup_response = tk.Label(self.frame, text = "Warmup:", font = ("Avenir", 20), justify = 'left')
-        warmup_response.grid(row = 4, column = 1, pady = 5, padx = 5)
+        warmup_response.grid(row = 4, column = 1, pady = 5, padx = 20, sticky = tk.W)
 
         cool_response = tk.Label(self.frame, text = "Cool Down:", font = ("Avenir", 20), justify = 'left')
-        cool_response.grid(row = 5, column = 1, pady = 5, padx = 5)
+        cool_response.grid(row = 5, column = 1, pady = 5, padx = 20, sticky = tk.W)
 
         self.ht = tk.StringVar(root, value = self.ht_val)
         ht_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.ht, width = 5)
-        ht_proposal.grid(row = 1, column = 2, pady = 5, padx = 5)
+        ht_proposal.grid(row = 1, column = 2, pady = 5, padx = 5, sticky = tk.W)
 
         self.lt = tk.StringVar(root, value = self.lt_val)
         lt_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.lt, width = 5)
-        lt_proposal.grid(row = 2, column = 2, pady = 5, padx = 5)
+        lt_proposal.grid(row = 2, column = 2, pady = 5, padx = 5, sticky = tk.W)
 
         self.between = tk.StringVar(root, value = self.between_val)
         between_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.between, width = 5)
-        between_proposal.grid(row = 3, column = 2, pady = 5, padx = 5)
+        between_proposal.grid(row = 3, column = 2, pady = 5, padx = 5, sticky = tk.W)
 
         self.warmup = tk.StringVar(root, value = self.warmup_val)
         warmup_proposal = tk.Entry(self.frame, font = ("Avenir",20), textvariable = self.warmup, width = 5)
-        warmup_proposal.grid(row = 4, column = 2, pady = 5, padx = 5)
+        warmup_proposal.grid(row = 4, column = 2, pady = 5, padx = 5, sticky = tk.W)
 
         self.cool = tk.StringVar(root, value = self.cool_val)
         cool_proposal = tk.Entry(self.frame, font = ("Avenir", 20), textvariable = self.cool, width = 5)
-        cool_proposal.grid(row = 5, column = 2, pady = 5, padx = 5)
+        cool_proposal.grid(row = 5, column = 2, pady = 5, padx = 5, sticky = tk.W)
 
-        next_button = tk.Button(self.frame, text = 'Ready?', font = ("Avenir", 20), justify = 'center', command = self.circuit_wrapper)
+        next_button = tk.Button(self.frame, text = 'Ready?', font = ("Avenir", 20), justify = 'center', command = self.countdown_init)
         next_button.grid(row = 6, column = 3)
 
         prev_button = tk.Button(self.frame, text = 'Previous', font = ("Avenir", 20), justify = 'center', command = self.window_1)
@@ -310,11 +272,15 @@ class Circuits(object):
             intensity = self.v2.get(),
             emp_prob = self.emp_prob,
             sec_emp_prob = self.sec_emp_prob)
-        self.workout_list = today_circuit.Name
-        self.workout_iter = 0
-        self.circuit_iter = 1
-        self.repeat_iter = 1
-        self.countdown_init()
+        # if not enough non-repeating workouts
+        if today_circuit.shape[0] < int(self.circ.get()) * int(self.set.get()):
+            self.error_popup()
+        else:
+            self.workout_list = today_circuit.Name
+            self.workout_iter = 0
+            self.circuit_iter = 1
+            self.repeat_iter = 1
+            self.window_2()
 
     def play_single_beep(self):
         mixer.init(44100)
@@ -322,6 +288,21 @@ class Circuits(object):
         mixer.music.play()
         time.sleep(0.7)
         mixer.music.stop()
+    
+    def error_popup(self):
+        self.window_1()
+        popup = tk.Toplevel()
+        popup.geometry("350x100")
+        popup.wm_title("Error Message")
+
+        message = tk.Label(popup, text = "Not enough non-repeating workouts.", font = ("Avenir", 20))
+        message.grid(row = 0, sticky = tk.W)  
+
+        message2 = tk.Label(popup, text = "Please select again.", font = ("Avenir", 20))
+        message2.grid(row = 1, sticky = tk.W)   
+
+        b = tk.Button(popup, text = "Okay", command = popup.destroy)
+        b.grid(row = 2,sticky = tk.E)
     
     # initialize the countdown from warmup
     def countdown_init(self):
@@ -491,7 +472,7 @@ class Circuits(object):
         title.grid(row = 3, column = 1, pady = 8)
 
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("900x600")
 root.title('Circuit Maker')
 app = Circuits()
 app.init(root)
